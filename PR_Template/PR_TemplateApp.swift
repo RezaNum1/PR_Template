5f95b8e70d45ccc5f4db480bee0c4bc85c2fe91a
Reza <rezafh19@gmail.com>
2023-03-09 10:01:27 +0700

    init

---
+++ PR_Template/PR_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PR_TemplateApp.swift
+//  PR_Template
+//
+//  Created by Reza Harris on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PR_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
